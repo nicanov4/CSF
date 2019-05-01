2a1b6ff3a29b027b3ffbfee82dca27d97878bb01
Nico <nivanov4@jhu.com>
2019-05-01 13:02:53 -0400

    l

--- cache.cpp
+++ cache.cpp
@@ -31,7 +31,7 @@ unsigned long long int missesPRF;
 
 int dir() {
   auto in = make_tuple(address, flag);
-  unsigned long long int index = address & 13;
+  index = address & 13;
   if (get<1>(cacheDIR[index]) != flag) {
     missesDIR++;
     cacheDIR[index] = in;
