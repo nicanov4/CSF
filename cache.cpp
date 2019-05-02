555f60f9eadc8931241d7e762212032f5a67c1e8
Nico <nivanov4@jhu.com>
2019-05-02 18:39:41 -0400

    f

--- cache.cpp
+++ cache.cpp
@@ -2,34 +2,34 @@
 #include <fstream>
 #include <stdio.h>
 #include <string.h>
-#include <vector>
+#include <list>
 #include <tuple>
 #include <map>
 using namespace std;
 
-unsigned long long int address;
+unsigned long long address;
 char flag;
 
-unsigned long long int hitsDIR;
-unsigned long long int missesDIR;
-map<int, tuple<unsigned long long int, char>> cacheDIR;
+unsigned long long hitsDIR;
+unsigned long long missesDIR;
+map<int, tuple<unsigned long long, char>> cacheDIR;
 
-unsigned long long int hitsASS;
-unsigned long long int missesASS;
+unsigned long long hitsASS;
+unsigned long long missesASS;
 int counterASS;
-vector<unsigned long long int> cacheASS;
+map<unsigned long long, char> cacheASS;
 
-unsigned long long int hitsSET;
-unsigned long long int missesSET;
+unsigned long long hitsSET;
+unsigned long long missesSET;
 
-unsigned long long int hitsBLK;
-unsigned long long int missesBLK;
+unsigned long long hitsBLK;
+unsigned long long missesBLK;
 
-unsigned long long int hitsNWA;
-unsigned long long int missesNWA;
+unsigned long long hitsNWA;
+unsigned long long missesNWA;
 
-unsigned long long int hitsPRF;
-unsigned long long int missesPRF;
+unsigned long long hitsPRF;
+unsigned long long missesPRF;
 
 int dir() {
   int index = address & 8191;
@@ -44,20 +44,8 @@ int dir() {
 }
 
 int ass() {
-  vector<unsigned long long int>::iterator curr;
-  curr = find(cacheASS.begin(), cacheASS.end(), address);
-  if (curr != cacheASS.end()) {
-    cacheASS.erase(curr);
-    cacheASS.insert(cacheASS.begin(), address);
-    hitsASS++;
-  } else {
-    if (counterASS >= 8192) {
-      cacheASS.pop_back();
-      cacheASS.insert(cacheASS.begin(), address);
-      counterASS++;
-    }
-    missesASS++;
-  }
+  list<unsigned long long> it;
+  it = find(cacheASS.begin(), cacheASS.end() address);
 }
 
 int main (int argc, char* argv[]) {
