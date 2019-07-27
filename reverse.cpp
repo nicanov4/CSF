58fd288dc2e84e89fe163853f7b0a052f70a709d
Nico <nivanov4@jhu.com>
2019-07-27 16:59:23 -0700

    reverse practice

---
+++ reverse.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <fstream>
+#include<stdio.h>
+#include<string.h>
+#include<array>
+
+using namespace std;
+
+
+int main (int argc, char* argv[]) {
+  string x = "pendeho"
+  for (i = x.size() - 1; i > -1; i--) {
+    cout << x[i];
+  }
+
+}
