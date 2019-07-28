eebc16f310f661ac8d522c97485a1ac9bdb8341d
Nico <nivanov4@jhu.com>
2019-07-27 17:19:33 -0700

    s

--- reverse.cpp
+++ reverse.cpp
@@ -11,11 +11,11 @@ int main (int argc, char* argv[]) {
   string x = "pendeho";
   int temp = x.size();
   int temp2 = 0;
-  cout << temp/2;
   for (int i = 0; i < (x.size()/2); i++) {
     temp2 = x[temp];
     x[temp] = x[i];
     x[i] = temp2;
+    temp-=1;
   }
   cout << x;
 
