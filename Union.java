8ea21bd3d196e11c1238e932eb94551818f03b16
Nico <nivanov4@jhu.com>
2019-09-25 20:28:16 -0400

    Union Test 1

---
+++ Union.java
@@ -0,0 +1,10 @@
+public class Union {
+
+  public Vector<Integer> union(Vector<Integer> a, Vector<Integer> b) {
+    return a;
+  }
+
+  public static void main(String[] args) {
+    System.out.println("Hunter sucks ASS");
+  }
+}
